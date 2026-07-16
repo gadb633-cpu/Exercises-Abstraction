b6f67d971710547f01c8162fea97ad7a1d6bd84b
gad-biton <gadb633@gmail.com>
2026-07-16 11:26:42 +0300

    added 4. Multiple Abstract Methods

--- basic_exercise.py
+++ basic_exercise.py
@@ -47,7 +47,35 @@ SkyEx = DroneDelivery("SkyEx")
 SpeedRiders = BikeDelivery("SpeedRiders")
 print(SkyEx.deliver(303))
 print(SpeedRiders.deliver(303))
+
+# 4. Multiple Abstract Methods
+class DeliveryMethod(ABC):
+    @abstractmethod
+    def deliver(self,order_id):
+        pass
+    @abstractmethod
+    def get_eta(self):
+        pass
+class BikeDelivery(DeliveryMethod):
+    def deliver(self,order_id):
+        return f"Order {order_id} — bike delivery."
+    def get_eta(self):
+        return f"ETA: {30}minutes"
         
+class DroneDelivery(DeliveryMethod):
+    def deliver(self,order_id):
+        return f"Order {order_id} — drone delivery."
+    def get_eta(self):
+        return f"ETA: {15}minutes"
+SkyEx = DroneDelivery()
+SpeedRiders = BikeDelivery()      
+print(SkyEx.deliver(1))
+print(SpeedRiders.deliver(1))
+print(SkyEx.get_eta())
+print(SpeedRiders.get_eta())
+
+    
+
 
 
 
