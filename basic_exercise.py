ec428d868a129ff9903b39fc34c10a9e660f1b6c
gad-biton <gadb633@gmail.com>
2026-07-16 11:00:09 +0300

    added 2. Two Delivery Types

--- basic_exercise.py
+++ basic_exercise.py
@@ -8,6 +8,26 @@ class BikeDelivery(DeliveryMethod):
     def deliver(self,order_id):
         return f"Order {order_id} delivered by bike."
 bike1 = BikeDelivery()
-print(bike1.deliver(101))  
+print(bike1.deliver(101)) 
+
+# 2. Two Delivery Types
+class DeliveryMethod(ABC):
+    @abstractmethod
+    def deliver(self,order_id):
+        pass
+class DroneDelivery(DeliveryMethod):
+    def deliver(self,order_id):
+        return f"Order {order_id} dropped by drone at your door."
+class CarDelivery(DeliveryMethod):
+    def deliver(self,order_id):
+        return f"Order {order_id} brought to your building by car."
+drone1 = DroneDelivery()
+print(drone1.deliver(202))
+car1 = CarDelivery()
+print(car1.deliver(202))
+
+        
+        
+
 
 
