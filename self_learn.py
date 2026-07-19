fd306db62a61d8109a30ca79bc28c05f03df57f9
gad-biton <gadb633@gmail.com>
2026-07-19 18:18:34 +0300

    added 2. Animal Factory

--- self_learn.py
+++ self_learn.py
@@ -25,3 +25,37 @@ print(notif1.get_sending_message())
 
 notif2 = create_notification("sms")
 print(notif2.get_sending_message())
+
+# 2. Animal Factory
+class Animal(ABC):
+    @abstractmethod
+    def get_sound(self):
+        pass
+
+class Dog(Animal):
+    def get_sound(self):
+        return "woof!"
+
+class Cat(Animal):
+    def get_sound(self):
+        return "meow!"
+
+class Bird(Animal):
+    def get_sound(self):
+        return "tweet!"
+
+def create_animal(animal_type):
+    if animal_type == "dog":
+        return Dog()
+    elif animal_type == "cat":
+        return Cat()
+    elif animal_type == "bird":
+        return Bird()
+    return 
+
+animal1 = create_animal("dog")
+print(animal1.get_sound())  
+animal2 = create_animal("cat")
+print(animal2.get_sound())  
+animal3 = create_animal("bird")
+print(animal3.get_sound())  
